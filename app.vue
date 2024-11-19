34ddd5af425d7a9ca9a727a114f11868b7c7c0e0
rofleksey <evilmorty@yandex.ru>
2024-11-20 01:49:29 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ModalsContainer />
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ModalsContainer } from 'vue-final-modal'
+
+</script>
